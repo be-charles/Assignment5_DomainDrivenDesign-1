52f6a1c4dc6b601102b3865975647ea46f6ac1d2
PhumiHoho <phumihoho@gmail.com>
2020-06-28 01:38:55 +0200

    Campus entity is created

--- src/main/java/org/entity/Campus.java
+++ src/main/java/org/entity/Campus.java
@@ -2,4 +2,52 @@ package org.entity;
 
 public class Campus {
     // Phumelela
+    private int campusCode;
+    private String name;
+
+    private Campus (Builder builder){
+        this.campusCode = builder.campusCode;
+        this.name = builder.name;
+    }
+
+    public static class Builder{
+        private int campusCode;
+        private String name;
+
+        public Builder setCampusCode(int campusCode){
+            this.campusCode = campusCode;
+            return this;
+        }
+
+        public Builder setName(String name){
+            this.name = name;
+            return this;
+        }
+
+        public Builder copy(Campus campus){
+            this.campusCode = campus.campusCode;
+            this.name = campus.name;
+            return this;
+        }
+
+        public Campus build(){
+            return new Campus(this);
+        }
+    }
+
+    public int getCampusCode() {
+        return campusCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Campus{" +
+                "campusCode=" + campusCode +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }

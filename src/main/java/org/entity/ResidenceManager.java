6dd499a6d5534422487095c1e671c866d75d228b
Luzuko Tshaka <luzukotshaka2@gmail.com>
2020-06-27 21:09:11 +0200

    ResidentManager Entity is created

--- src/main/java/org/entity/ResidenceManager.java
+++ src/main/java/org/entity/ResidenceManager.java
@@ -2,4 +2,76 @@ package org.entity;
 
 public class ResidenceManager {
     // Luzuko
+
+    private int managerId;
+    private String firstName, lastName;
+
+
+
+    private ResidenceManager(Builder builder)
+    {
+      this.managerId = builder.managerId;
+      this.firstName = builder.firstName;
+      this.lastName = builder.lastName;
+    }
+
+    public int getManagerId() {
+        return managerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "ResidenceManager{" +
+                "managerId=" + managerId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    public static class Builder{
+
+        private int managerId;
+        private String firstName, lastName;
+
+        public Builder setManagerId(int managerId){
+
+            this.managerId = managerId;
+            return this;
+        }
+
+        public Builder setFirstName(String firstName){
+
+            this.firstName = firstName;
+            return this;
+        }
+        public Builder setLastName(String lastName){
+
+            this.lastName = lastName;
+            return this;
+        }
+
+        public  Builder copy(ResidenceManager residenceManager){
+
+            this.managerId = residenceManager.managerId;
+            this.firstName = residenceManager.firstName;
+            this.lastName = residenceManager.lastName;
+            return this;
+        }
+
+
+        public ResidenceManager build(){
+
+            return new ResidenceManager(this);
+
+        }
+
+    }
 }
